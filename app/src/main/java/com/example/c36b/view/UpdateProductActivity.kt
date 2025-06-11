b0ff222e0faa1d459f4630a6d106964d53a117f1
Sain Rai <166593059+Sainrai0suiiiii@users.noreply.github.com>
2025-06-11 11:48:09 +0545

    update

--- app/src/main/java/com/example/c36b/view/UpdateProductActivity.kt
+++ app/src/main/java/com/example/c36b/view/UpdateProductActivity.kt
@@ -1,47 +1,133 @@
+
 package com.example.c36b.view
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.c36b.view.ui.theme.C36BTheme
+import com.example.c36b.repository.ProductRepositoryImpl
+import com.example.c36b.viewmodel.ProductViewModel
 
 class UpdateProductActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            C36BTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            UpdateProductBody()
+        }
+    }
+}
+
+@Composable
+fun UpdateProductBody() {
+
+    val repo = remember { ProductRepositoryImpl() }
+    val viewModel = remember { ProductViewModel(repo) }
+
+    val context = LocalContext.current
+    val activity = context as? Activity
+
+    var pName by remember { mutableStateOf("") }
+    var pPrice by remember { mutableStateOf("") }
+    var pDesc by remember { mutableStateOf("") }
+
+    val productId: String? = activity?.intent?.getStringExtra("productId")
+
+    val products = viewModel.products.observeAsState(initial = null)
+
+    LaunchedEffect(Unit) {
+        viewModel.getProductById(productId.toString())
+    }
+
+    pName = products.value?.productName ?: ""
+    pDesc = products.value?.productDesc ?: ""
+    pPrice = products.value?.productPrice.toString()
+
+    Scaffold { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            item {
+                OutlinedTextField(
+                    value = pName,
+                    onValueChange = {
+                        pName = it
+                    },
+                    placeholder = {
+                        Text("Product Name")
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                OutlinedTextField(
+                    value = pDesc,
+                    onValueChange = {
+                        pDesc = it
+                    },
+                    placeholder = {
+                        Text("Product Description")
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                OutlinedTextField(
+                    value = pPrice,
+                    onValueChange = {
+                        pPrice = it
+                    },
+                    placeholder = {
+                        Text("Product Price")
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Button(onClick = {
+                    var data = mutableMapOf<String, Any?>()
+
+                    data["productId"] = products.value?.productId
+                    data["productName"] = pName
+                    data["productDesc"] = pDesc
+                    data["productPrice"] = pPrice.toDouble()
+
+                    viewModel.updateProduct(products.value?.productId.toString(),data){
+                            success,message->
+                        if(success){
+                            activity?.finish()
+                        }else{
+                            Toast.makeText(context,message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }) {
+                    Text("Update Product")
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    C36BTheme {
-        Greeting("Android")
-    }
+fun preUpdate() {
+    UpdateProductBody()
 }
